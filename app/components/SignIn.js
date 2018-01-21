83a08e910922ec09ac466d588095a52b51084076
zhengshaoqiu <qq1143094348@gmail.com>
2018-01-21 23:57:38 +0800

    rename

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-
 import { login } from "../actions/login"; 
 import { connect } from "react-redux";
+
 //Row Badge
 import {
   Message,
